9e8301d6639b19260de99bdd11406c6267244e15
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-01-19 15:14:09 +0530

    ODD.C
    CHECK GIVEN NUMBER IS ODD OR EVEN

---
+++ ODD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("enter the number:");
+scanf("%d",&number);
+if(number%2==0)
+printf("%d is even:");
+else
+printf("%d is odd :");
+return 0;
+}
