132e50fe5570e04bcda480b806f6cffbbb74001f
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-01-21 15:50:10 +0530

    VOWEL.C
    CHECK THE GIVEN LETTER IS VOWEL OR CONSONANT

---
+++ VOWEL.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main();
+{
+char c;
+int c is lowercase vowel,uooercase vowel;
+printf("enter an alphabet:");
+scanf("%c",&c);
+if(c is lowercase vowel//is uppercase vowel)
+printf("%c is a vowel:");
+else
+printf("%c is a consonant:");
+return 0;
+}
