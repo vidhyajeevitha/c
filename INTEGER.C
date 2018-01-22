94af7b179bf5fc2925f61cc660294a11bd45e995
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-01-22 16:10:46 +0530

    INTGER.C
    COUNT NUMBERS OF AN INTEGER

---
+++ INTEGER.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+long long n;
+int count=0;
+printf("enter an integer:");
+scanf("%d",&n);
+{
+while(n!=0)
+n/=0;
+count n++;
+printf("number of digit:"%d,count);
+return 0;
+}
+}
