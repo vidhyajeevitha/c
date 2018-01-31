2e42f81190a560e2aa616d2d75add4fc120fafb3
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-01-31 16:35:19 +0530

    FACTORIAL.C
    CHECK THE FACTORIAL NUMBER

---
+++ FACTORIAL.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+long long factorial=1;
+printf("enter an integer:');
+scanf("%d",&n);
+if(n<0)
+{
+printf("factorial of number is:");
+else
+{
+for(i=1;i<n;++i)
+{
+factorial*=i;
+printf("not a factorial number":);
+}
+return(0);
+}
+}
+}
