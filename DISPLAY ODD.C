1e89828d291e6e99b746cd8197273fb265d176e6
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-01-31 16:05:36 +0530

    DISPLAY ODD.C
    DISPLAY THE ODD NUMBERS

---
+++ DISPLAY ODD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num,n=20,n=40;
+printf("print odd numbers in a given rangr mton:\n");
+for(num=m;num<=n;num++)
+{
+if(num/2==1)
+printf("%d",num);
+return(0);
+}
+}
