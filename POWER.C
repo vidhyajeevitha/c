11f44339081e0a17f68ffedd68f0a127c081f5d5
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-01-31 15:59:07 +0530

    POWER.C
    CALCULATE THE POWER OF NUMBER

---
+++ POWER.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int base,exponent;
+long long power=1;
+printf("enter base:");
+scanf("%d",&base);
+printf("enter exponent:");
+scanf("%d",&exponent:);
+for(i=1;i<=exponent;i++)
+{
+power=power*base;
+}
+printf("%d"%d=%d;base,exponent,power;
+retrn 0;
+}
