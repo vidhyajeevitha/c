23d76f8ae5e5a19706932e8e5f4e6f6572c4182d
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-02 16:25:53 +0530

    SORT.C
    PRINT THE SORTED ELEMENTS

---
+++ SORT.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int(int arr[],int1,int r,int x)
+{
+if(r>=1)
+{
+int min=1+(r-1)/2;
+if(arr[min]==X)
+return min,
+if(arr[min]>x)
+return(arr,1,min-1,X0;
+return(arr,min+1,r,x);
+}
+return-1;
+}
