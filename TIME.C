5933d7c057aa861bfd1e4f6634d5d5aee4dec148
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-02 16:51:22 +0530

    TIME.C
    CONVERT HOURS

---
+++ TIME.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<time.h>
+int main(void)
+{
+time t houe;
+char*h time string;
+hours=time(null);
+if(hours==((time t)_1)
+{
+(void) printf("failure to obtain hours");
+exit(EXIT FAILURE);
+h time string=h time(&hours);
+if(h time string==null)
+{
+(void)printf("convert hT FAILURE);
+}
+void printf("time is coverted %s",h time string);
+exit(EXIT SUCCESS);
+}
+}
