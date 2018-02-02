23fea26114d10296c066da9ef4002b86d0454e4a
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-02 15:47:00 +0530

    ARITHMETIC.C
    FIND THE SUM OF ARITHMETIC PROGRSSION

---
+++ ARITHMETIC.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int first,diff,terms,value,sum=0;
+printf("enter the no.of terms in ap series");
+scanf("%d",&first);
+printf("enter first term&common difference of ap series");
+scanf("%d%d",&first,&diff);
+value=first;
+printf("ap series");
+for)i=0;i<term,i++)
+{
+printf("%d",value);
+sum+=value;
+value=value+diff;
+}
+printf("sum pf the ap series till%d terms is %d",terms,sum);
+getch();
+return(0);
+}
