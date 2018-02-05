09a419f087fe948d8d4a8e36c6ed175610cabe5c
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-05 16:23:00 +0530

    SUB5.2.C
    SUBTRACTION OF NUMBERS

---
+++ SUB5.2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int  3 30,2 20,sub;
+printf("enter the first value:");
+scanf("%d",&n);
+printf("enter the second number:");
+scanf("%d",&n);
+sub=3.30-2.20;
+printf("the subtraction of two value is%d,%d ,sub);
+getch();
+}
