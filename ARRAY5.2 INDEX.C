7433545a83ea40803257ec48e8e2385d150db7fb
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-05 15:55:15 +0530

    ARRAY5.2.INDEXC
    PRINT ARRAY WITH INDEX

---
+++ ARRAY5.2 INDEX.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,arr[5],num;
+printf("enter no.of elements");
+scanf("%d",&num);
+printf("enter the values");
+scanf("%d",&arr[i]);
+for(i=0;i<num;i++)
+{
+for(j=0;j<num;j++)
+{
+printf("indes of each elements",arr[i]);
+}
+return 0;
+}
+}
