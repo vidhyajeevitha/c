557822b334e44e0d8705978595b6891833c192e8
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-05 16:08:04 +0530

    QUICK5.2.C
    PRINT QUICK AND SORTED ELEMENTS

---
+++ QUICK5.2.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void mergesort(inta[],inti);
+void merge(inta[],int i1,int j1);
+int main()
+{
+int a[30],n,i;
+printf("enter no.of elements:");
+scanf("%d".&n);
+printf("enter array elements:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a(i));
+merge sort(a,0,n-1);
+printf("sorted array:"0;for(i=0;i<n;j++)
+printf("%d",a[i]);
+return 0;
+}
+}
