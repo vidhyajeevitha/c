ca77144b7361e92d97cb622a040a52bd6a3aeb98
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-07 16:27:21 +0530

    SPECIAL7.2.C
    COUNT THE SPECIAL CHARACTERS

---
+++ SPECIAL7.2.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int amin()
+{
+char str[100];
+int count special char;
+count special char=0;
+printf("enter a string:");
+gets9str0;
+for(count=0;str[count]!=null;count++)
+{
+if(str[count]>=07&str[count]<'9');
+count special char++;
+}
+printf("5d special characters:"%d";count d)
+return 0;
+}
