559077b91e28bacb0ca8b16925ee48f2eae46efb
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-07 16:40:29 +0530

    DIGIT7.2.C
    COUNT THE DIGITS IN A GIVEN STRING

---
+++ DIGIT7.2.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char str[50];
+int count digit;
+count digit=0;
+printf("enter a string:");
+gets(str0;
+for(cout=0;str[count]!=null;count++)
+{
+if(str[count]>='0'&&str[count]<='9');
+count digit++;
+}
+printf("%d digit:"%d,count c)
+return 0;
+}
