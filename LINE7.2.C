b1d199e2db04549ba65fc2cfc8b282496ecb98f2
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-07 16:03:22 +0530

    LINE7.2.C
    COUNT THE NUMBER OF LINES IN GIVEN PARAGRAPH

---
+++ LINE7.2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char string[50];
+int a=0;count[20];
+printf("enter a string:");
+scanf("%s",&string);
+gets(string)
+while a[50]!='\0'
+if(i=0;i<0;i++)
+counta++
+printf("no.of lines in the given paragraph:",a,a++);
+return 0;
+}
