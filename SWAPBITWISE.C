180336238ea11f482080335d1db9a1c8241a8ab0
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-07 15:41:47 +0530

    SWAPBITWISE.C
    SWAP THE NUMBERS USING BIT WISE OPERATION

---
+++ SWAPBITWISE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i=34;
+int k=76;
+printf("value of i=%dk=%d before swapping:"i,k);
+i=i^k;
+k=i^k;
+i=i^k;
+printf("value of i=%dk=%d after swapping:"i,k);
+return o;
+}
