2b0b8cc1fa61c5e2a876c882eed5f26d339ecf69
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-07 16:33:28 +0530

    NUMERIC.C
    COUNT THE NUMERIC NUMBER

---
+++ NUMERIC.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char str[50];
+int count numeric number;
+numeric number=0;
+printf("enter a string:");
+gets(string);
+for(count=0;str[count]!=null;count++)
+{
+if(str[count]>='0'&&str[count]<='9');
+count numeric number++;
+}
+printf("%d numeric number :"%d;count d)
+return 0;
+}
