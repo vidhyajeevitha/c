c71e3eb12a24861cfc95dc33cd19030e3d2f376e
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-07 15:55:08 +0530

    SPACE.C
    COUNT THE  SPACES BETWEEN WORDS

---
+++ SPACE.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char string[50];
+int a=0;count[30];
+printf("enter the string:");
+scanf("%s",&string);
+gets(string);
+if(string[a]!='\0')
+{
+x=string[a]-'b';
+count[x];
+}
+for(i=0;i<30;i++)
+printf(count the space in the given string:",a='b',count[a]);
+return 0;
+}
