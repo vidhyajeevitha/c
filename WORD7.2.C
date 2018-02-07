662c3b6b5411fde3b24f72f8144835ff8c0e7546
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-07 15:49:01 +0530

    WORD7.2.C
    COUNT THE NUMBER OF WORDS

---
+++ WORD7.2.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char string[50];
+int i=0,count[20];
+printf("enter a string:");
+scanf("%s",&string);
+gets(string);
+while string[i]!='\0'
+{
+x=string[i]
+count[y]++;
+}
+for(i=0;i<20;i++)
+printf("count the number of words in x:"'a',count[i]);
+return 0;
+}
