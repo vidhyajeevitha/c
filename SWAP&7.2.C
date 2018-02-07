cc0cbe38e6529de3b328251416c057ecf256c523
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-07 15:35:28 +0530

    SWAP7.2.C
    SWAP THE TWO NUMBERS

---
+++ SWAP&7.2.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int x,y,temp;
+printf("value of x=%d y=%d before swapping:"x,y);
+temp=x;
+x=y;
+y=temp;
+printf("value of x=%dy=%d after swapping:"x,y);
+return 0;
+}
