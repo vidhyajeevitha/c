6bd4e6cc59d242b1cee1ee29f3e864d8a21ec23e
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-07 16:15:57 +0530

    MAXIMUM7.2.C
    COUNT THE MAXIMUM AMONG 10 NUMBERS

---
+++ MAXIMUM7.2.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int array[50],max ,i;
+printf("enter the no.of elements:");
+scanf("%d",&n);
+printf(enter %d integers");
+for(i=0;i<=0;i++)
+{
+scanf("%d",&array[i]);
+max=array[10];
+if (array[i]>maximum)
+{
+max=array[i];
+return 0;
+}
+}
+}
