1f75afb9d627e3794f57fb3bced1616ce5f61b52
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-07 16:46:15 +0530

    NEXTNUMBER.C
    COUNT THE NEXT NUMBER

---
+++ NEXTNUMBER.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+   int n,i,j,flag=0;
+
+   printf("enter the num\n");
+
+   scanf("%d",&n);
+
+   for(i=n+1;i<=100;i++)
+   {
+
+      flag=0;
+
+      for(j=2;j<i;j++)
+      {
+         if(i%j==0)
+         {
+            flag=1;
+            break;
+         }
+      }
+
+      if(flag==0)
+      {
+         printf("next numberis:%d",i);
+         break;
+      }
+   }
+
+  return 0;
+
+}
