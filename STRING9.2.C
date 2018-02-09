1a9ec6c8ae62a1a87b2353c96a6cfdd981322bad
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-09 16:20:50 +0530

    STRING9.2.C
    PRINT THE STRING N TIMES

---
+++ STRING9.2.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,stat();
+    char str[500];
+    for(i=1;i<=3;i++)
+        stat();
+    getch();
+}
+int stat()
+{
+    staticint x = 0;
+    x = x + 1;
+    printf("print the string n times");
+    return(0);
