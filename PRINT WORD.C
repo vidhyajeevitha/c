ae6f9b115b935ba5b6639bc03c268c6c5ff2bb40
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-09 16:42:43 +0530

    PRINT WORD.C
    PRINT THE WORD N TIMES

---
+++ PRINT WORD.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int 4;
+scanf("%d",&4);
+switch(4);
+{
+case 1:
+printf("one");
+break;
+case 2:
+printf("two");
+break;
+case 3:
+printf("three");
+break;
+case 4:
+printf("four");
+break;
+return 0;
+}
+}
