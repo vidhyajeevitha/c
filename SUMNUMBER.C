6508b6ee81c5b2766db7164ef79d5c73daf0c233
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-09 16:33:41 +0530

    SUMNUMBER.C
    PRINT THE SUM OF NUMBERS

---
+++ SUMNUMBER.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int 2 Numbers j, i, Sum = 0;
+  
+  printf("Please Entertwo Value");
+  scanf("%d", &Number);
+  
+  for(i = 1; i <= Number; i++)
+  {
+     Sum = j + i;
+  }
+  
+  printf("Sum of Numbers = %d", Sum);
+  return 0;
+}
