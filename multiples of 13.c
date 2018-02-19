b8b08acbcdc79905271b7acb32630ec9a71b590e
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-19 16:38:49 +0530

    multiples of 13.c
     find the multiples of 13

---
+++ multiples of 13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d\n",&a);
+	printf("%d\n",a);
+	if(a%13==0)
+	{
+		printf("mulitiple of 13");
+	}
+	else
+		{
+		printf("not multiple of 13");
+	}
+	return 0;
+}
