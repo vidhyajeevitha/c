87ffb1e46e6999ddcc8108e81ab175eea7ee85f2
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-19 16:20:18 +0530

    left right.c
    left right operation

---
+++ left right.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+printf("enter the number:");
+scanf("%d",&x);
+printf("left and right numbers:");
+scanf("%d%d",&y,&z);
+if(x>y&&a<z)
+{
+	printf("yes");
+}
+else
+{
+	printf("no");
+}
+return 0;
+}
