cbc9d122636c5db7c85468b42c5adfd69a0a928e
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-23 15:49:33 +0530

    isogram.c
    find the isogram

---
+++ isogram.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+
+	char z[50];
+	int i,j,n,count;
+    printf("\n Enter the word:");
+    scanf("%s",z);
+    n=strlen(z);
+    for(i=0;i<1;i++)
+    for(j=i+1;j<1;j++)
+    if(z[i]==a[j])
+    {
+        count=1;break;
+    }
+    if(count==0)
+    {
+        printf("\n It is isogram");
+    }
+    else
+    {
+        printf("\n It is not isogram");
+    }
+  getch();
+}
+
+ 
+
