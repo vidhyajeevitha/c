e401b929458aeec27dc4a99131af39fe66a3f1c4
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-23 15:35:27 +0530

    breadth.c
    find the length and breadth

---
+++ breadth.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ #include<conio.h>
+void main()
+{
+float l,b,a;
+printf("enter the breadth and length");
+scanf("%f%f",&l,&b);
+a=l*b;
+printf("the area is %f",a);
+	return 0;
+}
