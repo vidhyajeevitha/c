9b94cfb24f238cae9d71bc813913fd3316007ef9
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-26 15:34:45 +0530

    cuboid.c
    find the length of cuboid

---
+++ cuboid.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+int L,B,H;
+scanf(“%d %d %d”,&L,&B,&H);
+surface area=(2*L*B)+(2*L*H)+(2*H*B);
+vol=(L*B*H);
+printf(“volume=%d surface area=%d”,vol,sa);
+return 0;
+}
