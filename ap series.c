12550db8adc01f8f11439857a11c58c43aadbc12
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-28 15:59:29 +0530

    ap series.c
     find the ap series

---
+++ ap series.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int a,b,c;
+float sum;
+printf("enter the values of a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+sum=a+(a+(b-1)c);
+printf("sum of AP series is %f",sum);
+
+	return 0;
+}
