a910efba20e4836b35b141df7fb289d7c65f3f06
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-28 16:34:05 +0530

    string 28.c
    string reverse operation

---
+++ string 28.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+
+
+int main(void) {
+
+
+Str a[100];
+printf("Enter the string");
+gets(str)
+string rev(str);
+printf("\nthe reverse string is :",%s a);
+
+	return 0;
+}
