76a77ddc1fc8de34e97fc860f017b9823698193f
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-02-28 15:33:56 +0530

    character.c
    print the character

---
+++ character.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+
+	int num,x,b=64;
+	printf("Enter the number");
+	scanf("%d",&num);
+	for(x=0;x<num;x++)
+	{
+	 b++;
+	}
+	printf("%c",b);
+
+	return 0;
+}
+
