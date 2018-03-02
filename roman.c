cd35a86d3364ef22b9e766b6d08dc8928fb15a8a
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-03-02 15:47:08 +0530

    roman.c
    convert roman number into integer

---
+++ roman.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+    char a[100];
+        int p,i,v,x;
+    gets(a);
+    for(i=0 ; i < a[i] != '\0' ; i++)
+{
+	if(a[i] == 'i')
+{
+	p= p+1;
+}
+else if( a[i] == 'v') 
+{
+	p=p+5;
+}
+	else if( a[i] == 'x')
+{
+		p= p + 10;	
+	}
+}
+ for(i=0 ; i < a[i] != '\0' ; i++)
+{
+	if(a[i] == 'i' && (a[i+1] == 'v' ||   a[i+1]== 'x'))
+	p = p-2;
+	}   
+	printf("%d\n",p);  
+	return 0;
+}
