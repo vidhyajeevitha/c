2d420f06c94270a9db4c23494735335bf36b545d
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-03-02 11:13:52 +0000

    prime number 2.c
    find the prime number

---
+++ prime number 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) 
+{
+int i,z;
+flag=0;
+printf("enter a number:");
+scanf("%d",&number);
+for(i=2;i<=n\2;++i)
+{
+if(n%i=0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+
+printf("%d is a prime number:");
+}
+	return 0;
+}
