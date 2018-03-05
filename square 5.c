1c13d3477e188608e519e2f2314e386030ac037b
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-03-05 15:52:28 +0530

    square.c
    find the square of digits

---
+++ square 5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+	int a;
+	int c,b,sum=0;
+	scanf("%d",&n);
+	while(n>0)
+	{
+		a=n%10;
+		c=b*b;
+		sum=sum+c;
+		a=a/10;
+	}
+	printf("%d",sum);
+	return 0;
+}
