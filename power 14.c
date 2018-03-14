c1c66d902cbdcb98dda646fa0e7c8caaad9e08d7
vidhyajeevitha <35295026+vidhyajeevitha@users.noreply.github.com>
2018-03-14 16:13:40 +0530

    power 14.c
    power of 2

---
+++ power 14.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+  int main(void)
+{
+    int i,a,b;
+    printf("enter the number");
+    scanf("%d",&n);
+    a=sqrt(i);
+    b=i%a;
+    if(b==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
